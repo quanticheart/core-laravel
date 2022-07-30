9296ca3cb4f99cd9035e1a145c28692256b5de3f
Jonn Alves <jonatas.alsantos@jsl.com.br>
2022-07-30 14:53:50 -0300

    first commit

---
+++ src/providers/QuanticHeartProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Quanticheart\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class QuanticHeartProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
