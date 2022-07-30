42960a9fe5347ad07d98193eb003e305980ad80b
Jonn Alves <jonatas.alsantos@jsl.com.br>
2022-07-30 16:30:36 -0300

    update library

--- src/Providers/HelperServiceProvider.php
+++ src/Providers/HelperServiceProvider.php
@@ -16,6 +16,10 @@ class HelperServiceProvider extends ServiceProvider
         foreach (glob(app_path() . '/Helpers/*.php') as $file) {
             require_once($file);
         }
+
+        foreach (glob(__DIR__ . '/../Helpers/*.php') as $file) {
+            require_once($file);
+        }
     }
 
     /**
